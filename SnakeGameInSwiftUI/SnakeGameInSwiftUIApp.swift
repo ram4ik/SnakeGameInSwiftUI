150276eb6bb2762a13da5c8526e1d3a437b49f6c
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-01-04 17:53:21 +0200

    Initial Commit

---
+++ SnakeGameInSwiftUI/SnakeGameInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameInSwiftUIApp.swift
+//  SnakeGameInSwiftUI
+//
+//  Created by Ramill Ibragimov on 04.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
